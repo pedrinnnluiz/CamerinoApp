7ef08c08552ad69aba98908a51de4212a62cd697
Pedrão <Pedro@DESKTOP-N193HP2>
2026-04-06 15:06:35 -0300

    CadastroPage - Verificações

--- CamerinoApp/CadastroPage.xaml.cs
+++ CamerinoApp/CadastroPage.xaml.cs
@@ -1,3 +1,6 @@
+
+using System.Threading.Tasks;
+
 namespace CamerinoApp;
 
 public partial class CadastroPage : ContentPage
@@ -6,4 +9,36 @@ public partial class CadastroPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void CadastrarClicked_Clicked(object sender, EventArgs e)
+	{
+
+        if (string.IsNullOrEmpty(SenhaEntry.Text)
+        || string.IsNullOrEmpty(EmailEntry.Text))
+        {
+            await DisplayAlert("Alerta", "Preencha todos os dados", "Ok");
+            return;
+        }
+
+
+        if(!EmailEntry.Text.Contains("@gmail.com")
+           || !EmailEntry.Text.Contains("hotmail.com")
+           ||  !EmailEntry.Text.Contains("@outlook.com "))
+        {
+            await DisplayAlert("Alerta", "Seu email deve ter enderaçamento", "Ok");
+            return;
+        }
+
+
+        if (!(SenhaEntry.Text.Length < 8))
+        {
+            await DisplayAlert("Alerta", " Sua senha deve conter 8 caracteres", "Ok"); 
+            return;
+        }
+        else
+        {
+            await DisplayAlert("Sucesso", "Cadastro feito com sucesso", "Ok");
+        }
+
+    }
 }
